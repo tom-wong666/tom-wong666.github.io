222cf8955522d0903cc50503de8be963f4bda719
Tom Wong <3766639606@qq.com>
2018-09-29 23:28:08 +0800

    test for next.html

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ let test = document.getElementById("test");
 test.onclick=function(){
     alert("hello")
 }
+let next = document.getElementById("next");
+next.onclick=function(){
+    location.href="next.html"
+}
