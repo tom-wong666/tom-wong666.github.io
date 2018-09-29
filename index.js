ba9741908fd6fa8c413d74ba91daa5516724c311
Tom Wong <3766639606@qq.com>
2018-09-29 23:33:09 +0800

    translate xiaoa project to my github pages

--- index.js
+++
@@ -1,8 +0,0 @@
-let test = document.getElementById("test");
-test.onclick=function(){
-    alert("hello")
-}
-let next = document.getElementById("next");
-next.onclick=function(){
-    location.href="next.html"
-}
